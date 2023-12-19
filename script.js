b28eaf812356f0179811ffb128eff970643a6699
Anderson2ilv4 <silvaanderson124@gmail.com>
2023-12-18 21:49:41 -0300

    trnaslation-update

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const ptButton = document.getElementById('pt-br')
-const enButton = document.getElementById('en')
+const ptButton = document.getElementById('ptButton')
+const enButton = document.getElementById('enButton')
 
-function alerta() {
-    console.log('TEste')
+function ptTranslation() {
+    location.reload()
 }
 
-function english() {
+function enTranslation() {
+    var title = document.getElementById('title')
+    var presentation = document.getElementById('presentation')
+    var fCourses = document.getElementById('finishedCourses')
+    var course1 = document.getElementById('course1')
+    var doing1 = document.getElementById('doing1')
+    var inProgress = document.getElementById('inProgress')
+    var videos = document.getElementById('videos')
+    var projects = document.getElementById('projects')
+    var subscribe = document.getElementById('subscribe')
+    var madeBy = document.getElementById('madeBy')
+
+    presentation.innerHTML = 'Next we have some studied technologies, some projects and other resources:'
+    title.innerHTML = 'Portfolio'
+    course1.innerHTML = 'Computer Network Technician - Federal Institute of Science and Technology of Pernambuco 2017-2018.2'
+    fCourses.innerHTML = 'Finished courses:'
+    doing1.innerHTML = 'Software Engineer - UNOPAR 2023.2-'
+    inProgress.innerHTML = 'In progress:'
+    videos.innerHTML = 'Videos'
+    projects.innerHTML = 'Projects'
+    subscribe.innerHTML = 'Subscribe'
+    madeBy.innerHTML = 'Made by'
+    
     console.log('English')
+
 }
 
-ptButton.onclick = alerta
-enButton.onclick = english
+ptButton.onclick = ptTranslation
+enButton.onclick = enTranslation
+
 
