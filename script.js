88285d7297ee53bba553f3c25afd209555b37098
Anderson2ilv4 <silvaanderson124@gmail.com>
2023-12-22 20:33:05 -0300

    translation-update

--- script.js
+++ script.js
@@ -2,37 +2,30 @@ const ptButton = document.getElementById('ptButton')
 const enButton = document.getElementById('enButton')
 
 function ptTranslation() {
-    location.reload()
+    var title = document.getElementById('title').innerHTML = 'Portfólio'
+    var presentation = document.getElementById('presentation').innerHTML = 'A seguir são as tecnologias estudadas, alguns projetos e outros recursos:'
+    var fCourses = document.getElementById('finishedCourses').innerHTML = 'Cursos concluídos:'
+    var course1 = document.getElementById('course1').innerHTML = 'Técnico em Redes de Computadores - Instituto Federal de Ciência e Tecnologia de Pernambuco 2017-2018.2'
+    var doing1 = document.getElementById('doing1').innerHTML = 'Engenharia de Software - UNOPAR 2023.2 -'
+    var inProgress = document.getElementById('inProgress').innerHTML = 'Em andamento:'
+    var videos = document.getElementById('videos').innerHTML = 'Vídeos'
+    var projects = document.getElementById('projects').innerHTML = 'Projetos'
+    var subscribe = document.getElementById('subscribe').innerHTML = 'Inscreva-se'
+    var madeBy = document.getElementById('madeBy').innerHTML = 'Feito por'
 }
 
 function enTranslation() {
-    var title = document.getElementById('title')
-    var presentation = document.getElementById('presentation')
-    var fCourses = document.getElementById('finishedCourses')
-    var course1 = document.getElementById('course1')
-    var doing1 = document.getElementById('doing1')
-    var inProgress = document.getElementById('inProgress')
-    var videos = document.getElementById('videos')
-    var projects = document.getElementById('projects')
-    var subscribe = document.getElementById('subscribe')
-    var madeBy = document.getElementById('madeBy')
-
-    presentation.innerHTML = 'Next we have some studied technologies, some projects and other resources:'
-    title.innerHTML = 'Portfolio'
-    course1.innerHTML = 'Computer Network Technician - Federal Institute of Science and Technology of Pernambuco 2017-2018.2'
-    fCourses.innerHTML = 'Finished courses:'
-    doing1.innerHTML = 'Software Engineer - UNOPAR 2023.2-'
-    inProgress.innerHTML = 'In progress:'
-    videos.innerHTML = 'Videos'
-    projects.innerHTML = 'Projects'
-    subscribe.innerHTML = 'Subscribe'
-    madeBy.innerHTML = 'Made by'
-    
-    console.log('English')
-
+    var title = document.getElementById('title').innerHTML = 'Portfolio'
+    var presentation = document.getElementById('presentation').innerHTML = 'Next we have some studied technologies, some projects and other resources:'
+    var fCourses = document.getElementById('finishedCourses').innerHTML = 'Finished courses:'
+    var course1 = document.getElementById('course1').innerHTML = 'Computer Network Technician - Federal Institute of Science and Technology of Pernambuco 2017-2018.2'
+    var doing1 = document.getElementById('doing1').innerHTML = 'Software Engineer - UNOPAR 2023.2-'
+    var inProgress = document.getElementById('inProgress').innerHTML = 'In progress:'
+    var videos = document.getElementById('videos').innerHTML = 'Videos'
+    var projects = document.getElementById('projects').innerHTML = 'Projects'
+    var subscribe = document.getElementById('subscribe').innerHTML = 'Subscribe'
+    var madeBy = document.getElementById('madeBy').innerHTML = 'Made by'
 }
 
 ptButton.onclick = ptTranslation
 enButton.onclick = enTranslation
-
-
